9335b480b59587251cacd0f8eba9820cc62098ab
xibeijia <xibeijia@xibeijia-PC>
2019-07-18 00:28:17 +0800

    session

--- src/com/how2java/test/TestHibernate.java
+++ src/com/how2java/test/TestHibernate.java
@@ -1,56 +1,94 @@
 package com.how2java.test;
- 
+
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
- 
-import com.how2java.pojo.Category;
- 
-public class TestHibernate {
-    public static void main(String[] args) {
-        SessionFactory sf = new Configuration().configure().buildSessionFactory();
- 
-        Session s = sf.openSession();
-        s.beginTransaction();
-        Category c = (Category) s.get(Category.class, 1);
- 
-        System.out.println("log1");
-        System.out.println(c.getProducts());
-        System.out.println("log1");
- 
-        s.getTransaction().commit();
-        s.close();
-        sf.close();
-    }
-}
+import org.hibernate.criterion.Restrictions;
 
-
-/*package com.how2java.test;
-  
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
- 
 import com.how2java.pojo.Product;
-  
+
 public class TestHibernate {
-    public static void main(String[] args) {
-        SessionFactory sf = new Configuration().configure().buildSessionFactory();
-  
-        Session s = sf.openSession();
-        s.beginTransaction();
-     
-        Product p = (Product)s.load(Product.class, 1);
-        System.out.println("log1");
-        System.out.println(p.getName());
-        System.out.println("log2");
- 
-        s.getTransaction().commit();
-        s.close();
-        sf.close();
-    }
+	public static void main(String[] args) {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session s = sf.openSession();
+		s.beginTransaction();
+
+		String name = "iphone";
+
+		Criteria c = s.createCriteria(Product.class);
+		c.add(Restrictions.like("name", "%" + name + "%"));
+		c.setFirstResult(2);
+		c.setMaxResults(5);
+
+		List<Product> ps = c.list();
+		for (Product p : ps) {
+			System.out.println(p.getName());
+
+		}
+
+		s.getTransaction().commit();
+		s.close();
+		sf.close();
+	}
 }
-*/
+
+/*
+ * package com.how2java.test;
+ * 
+ * import org.hibernate.Session; import org.hibernate.SessionFactory; import
+ * org.hibernate.cfg.Configuration;
+ * 
+ * import com.how2java.pojo.Category;
+ * 
+ * public class TestHibernate { public static void main(String[] args) {
+ * SessionFactory sf = new Configuration().configure().buildSessionFactory();
+ * Session s = sf.openSession(); s.beginTransaction(); Category c = (Category)
+ * s.get(Category.class, 3); s.delete(c); s.getTransaction().commit();
+ * s.close(); sf.close(); } }
+ */
+
+/*
+ * package com.how2java.test;
+ * 
+ * import org.hibernate.Session; import org.hibernate.SessionFactory; import
+ * org.hibernate.cfg.Configuration;
+ * 
+ * import com.how2java.pojo.Category;
+ * 
+ * public class TestHibernate { public static void main(String[] args) {
+ * SessionFactory sf = new Configuration().configure().buildSessionFactory();
+ * 
+ * Session s = sf.openSession(); s.beginTransaction(); Category c = (Category)
+ * s.get(Category.class, 1);
+ * 
+ * System.out.println("log1"); System.out.println(c.getProducts());
+ * System.out.println("log1");
+ * 
+ * s.getTransaction().commit(); s.close(); sf.close(); } }
+ */
+
+/*
+ * package com.how2java.test;
+ * 
+ * import org.hibernate.Session; import org.hibernate.SessionFactory; import
+ * org.hibernate.cfg.Configuration;
+ * 
+ * import com.how2java.pojo.Product;
+ * 
+ * public class TestHibernate { public static void main(String[] args) {
+ * SessionFactory sf = new Configuration().configure().buildSessionFactory();
+ * 
+ * Session s = sf.openSession(); s.beginTransaction();
+ * 
+ * Product p = (Product)s.load(Product.class, 1); System.out.println("log1");
+ * System.out.println(p.getName()); System.out.println("log2");
+ * 
+ * s.getTransaction().commit(); s.close(); sf.close(); } }
+ */
 
 /*
  * package com.how2java.test;
